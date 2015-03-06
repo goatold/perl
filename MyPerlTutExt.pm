0f99fc47cfbe36dcb7b58709907dfefbc100dfc8
Leo Wang <goatold@gmail.com>
2015-03-06 11:55:05 +0800

    add process demo

--- MyPerlTutExt.pm
+++ MyPerlTutExt.pm
@@ -19,6 +19,8 @@ sub new {
 }
 
 # extend more demo func
+
+# show case database operation with SQLite
 sub demo_db {
     my ($self) = @_;
     my $mfn = (caller(0))[3];
@@ -31,7 +33,7 @@ sub demo_db {
     # connect to DB
     my $dbh = DBI->connect($dsn, $uid, $pwd, { RaiseError => 1 }) 
               or die $DBI::errstr;
-    $dbh->trace(4); # set highest trace level
+    $dbh->trace(0); # set trace level. the highest is 4
     # create table
     my $sqlstr = qq{
 CREATE TABLE test (
@@ -72,3 +74,52 @@ CREATE TABLE test (
     $dbh->do("DROP TABLE IF EXISTS test;");
     $dbh->disconnect();
 }
+
+# run process in perl
+sub sigh_int {print "sig int handled\n";}
+sub sigh_hup {print "sig hup handled\n";}
+
+sub demo_proc {
+    my ($self) = @_;
+    my $mfn = (caller(0))[3];
+    $self->prog($mfn);
+    # backsticks
+    print "time now is ".`date`;
+    # system calls
+    my @options = ("-l", "-a", "-t");
+    system("ls", @options);
+    # file handle as pipe
+    open(WHO, "who|");
+    my @who = <WHO>;
+    open(USORT, "|sort -u");
+    print USORT @who;
+    close(WHO);
+    close(USORT);
+    # fork process
+    my $pid;
+    if (!defined($pid = fork())) {
+        warn "fork failed: $!";
+        return -1;
+    } elsif ($pid == 0) {
+        print "crying kid\n";
+        exec('echo "kid pid $$ sleeping";sleep 2');
+    } else {
+        print "yelling parent\n";
+        my $rv = waitpid($pid, 0);
+        print "waitpid returned $rv: Kid pid($pid) status: $?\n";
+    }
+    # signal handling
+    my $sighup_h = $SIG{'HUP'};
+    use sigtrap qw(handler sigh_int INT);
+    $SIG{'HUP'} = \&sigh_hup;
+    # send signal to myslef
+    kill('INT', $$);
+    kill('HUP', $$);
+    $SIG{'INT'} = 'IGNORE';
+    print "sigin ignored\n";
+    kill('INT', $$);
+    # restore old sig handler
+    $SIG{'HUP'} = $sighup_h;
+    kill('HUP', $$);
+}
+
